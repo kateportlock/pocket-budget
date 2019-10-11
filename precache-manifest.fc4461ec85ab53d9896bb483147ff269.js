e4bc07935cf3aad1276358e45d8c0a07b7b61a2c
kate-gill <kategill@email.com>
2019-10-11 12:09:31 +0100

    Updates

---
+++ precache-manifest.fc4461ec85ab53d9896bb483147ff269.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b225e68c9f29c3d95da6bba416d3487",
+    "url": "/pocket-budget/index.html"
+  },
+  {
+    "revision": "a4d2676f80c932a4c80b",
+    "url": "/pocket-budget/static/css/main.1864b028.chunk.css"
+  },
+  {
+    "revision": "a79f6009efa3755a34ea",
+    "url": "/pocket-budget/static/js/2.7ec084c4.chunk.js"
+  },
+  {
+    "revision": "a4d2676f80c932a4c80b",
+    "url": "/pocket-budget/static/js/main.ad44414a.chunk.js"
+  },
+  {
+    "revision": "21435fb1dd98cf45cbee",
+    "url": "/pocket-budget/static/js/runtime~main.95e774a8.js"
+  }
+]);
